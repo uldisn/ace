8acb4dc0c0fc56ac466c95eb7876b5f5109b5f07
Uldis Nelsons <uldisnelsons@gmail.com>
2015-02-15 11:25:28 +0200

    TbAceDetailView added css value height

--- widgets/TbAceDetailView.php
+++ widgets/TbAceDetailView.php
@@ -1,53 +1,45 @@
 <?php
 
-class TbAceDetailView extends CDetailView
-{
+class TbAceDetailView extends CDetailView {
 
     public $tagName = 'div';
-    public $htmlOptions = array('class'=>'profile-user-info profile-user-info-striped');
+    public $htmlOptions = array('class' => 'profile-user-info profile-user-info-striped');
     public $itemTemplate = '
                 <div class="profile-info-row"><div class="profile-info-name">{label}</div>
                 <div class="profile-info-value">{value}</div></div>';
     public $label_width = 120;
 
-	/**
-	 * @var string the URL of the CSS file used by this detail view.
-	 * Defaults to false, meaning that no CSS will be included.
-	 */
-	public $cssFile = false;
+    /**
+     * @var string the URL of the CSS file used by this detail view.
+     * Defaults to false, meaning that no CSS will be included.
+     */
+    public $cssFile = false;
 
- 	public function init()
-	{
-		parent::init();
-        
-        Yii::app()->clientScript->registerCss($this->id.'fix_detail_view', ' 
-            #'.$this->id.' .profile-info-name {width: '.$this->label_width.'px;}
-            #'.$this->id.' .profile-info-value {margin-left: '.$this->label_width.'px;}
-        ');        
-        
-        foreach($this->attributes as $k => $attribute)
-		{
-            if(!isset($attribute['external_link'])){
+    public function init() {
+        parent::init();
+
+        Yii::app()->clientScript->registerCss($this->id . 'fix_detail_view', ' 
+            #' . $this->id . ' .profile-info-name {width: ' . $this->label_width . 'px;}
+            #' . $this->id . ' .profile-info-value {margin-left: ' . $this->label_width . 'px;height: 20px;}
+        ');
+
+
+        foreach ($this->attributes as $k => $attribute) {
+            if (!isset($attribute['external_link'])) {
                 continue;
             }
-            if(empty($attribute['value_id'])){
+            if (empty($attribute['value_id'])) {
                 continue;
             }
-            $this->attributes[$k]['value'] .=  '&nbsp;'
-                    .CHtml::link(
-                            '<i class="icon-external-link"></i>',
-                            $attribute['external_link'],
-                            array(
-                                'target'=>'_blank',
-                                'title'=>$attribute['external_title'],
-                                'data-toggle'=>'tooltip',
-                                )
-                            );
-        }            
-        
+            $this->attributes[$k]['value'] .= '&nbsp;'
+                    . CHtml::link(
+                            '<i class="icon-external-link"></i>', $attribute['external_link'], array(
+                        'target' => '_blank',
+                        'title' => $attribute['external_title'],
+                        'data-toggle' => 'tooltip',
+                            )
+            );
+        }
+    }
 
-	}
-   
-    
-    
 }
